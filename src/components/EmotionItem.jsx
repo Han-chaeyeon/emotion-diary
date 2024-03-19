56f5ccb4c2fa39263cc54c0336a2800ab7060e93
hanchaeyeon <whalfy_02@naver.com>
2024-03-19 14:49:35 +0900

    emotion-diary

---
+++ src/components/EmotionItem.jsx
@@ -0,0 +1,18 @@
+import { getEmotionImage } from "../util/get-emotion-image";
+import "../css/EmotionItem.css";
+
+const EmotionItem = ({ emotionId, emotionName, isSeleted, onClick }) => {
+  return (
+    <div
+      onClick={onClick}
+      className={`EmotionItem ${
+        isSeleted ? `EmotionItem_on_${emotionId}` : ""
+      }`}
+    >
+      <img className="emotion_img" src={getEmotionImage(emotionId)} />
+      <div className="emotion_name">{emotionName}</div>
+    </div>
+  );
+};
+
+export default EmotionItem;
